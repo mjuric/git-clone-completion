d9973c99248501a8009d5d778b54b08ff77b66d7
Mario Juric <mjuric@astro.washington.edu>
2019-12-17 00:52:04 -0800

    git-get-completion.bash: initial 'git clone' autocomplete support

--- git-get-completion.bash
+++ git-get-completion.bash
@@ -100,59 +100,12 @@ _get_repo_list()
 	REPOS=( $( cat "$CACHE" 2>/dev/null ) )
 }
 
-_git_get_unit_test()
+_complete_url()
 {
-	for cmd in "git-get" "git get"; do
-		for opts in "" "-s --long"; do
-			# test succesful completions
-			COMPREPLY_TRUE=( "mjuric/lsd " "mjuric/lsd-setup " )
-			for url in "mjuric/lsd" "git@github.com:mjuric/lsd" "https://github.com/mjuric/lsd" "http://github.com/mjuric/lsd"; do
-				COMP_WORDS=($cmd $opts $url)
-				COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1)) _git_get
-				# compare the output vs expectation
-				diff=$(diff <(printf "%s\n" "${COMPREPLY_TRUE[@]}") <(printf "%s\n" "${COMPREPLY[@]}"))
-				[[ -n "$diff" ]] && { echo "[🛑] error: ${COMP_WORDS[@]} returned wrong completions" $'\n' "$diff"; return; }
-				echo "[✔] ${COMP_WORDS[@]}"
-			done
+	URL="$1"
 
-			# test completions that should fail
-			for url in "./mjuric/lsd" "a/b/c" "git@github.com"; do
-				COMP_WORDS=($cmd $opts $url)
-				COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1)) _git_get
-				[[ ${#COMPREPLY[@]} == 0 ]] || { echo "[🛑] error: ${COMP_WORDS[@]} should've returned zero completions"; printf "%s\n" "${COMPREPLY[@]}"; return; }
-				echo "[✔] ${COMP_WORDS[@]}"
-			done
-		done
-	done
-}
-
-# git's autocompletion scripts will automatically invoke _git_get() for 'get' subcommand
-_git_get()
-{
-	# are we invoked as standalone, or git subcommand?
-	IARG=1
-	[[ ${COMP_WORDS[0]} == "git" ]] && IARG=2
-
-	COMPREPLY=()
-
-	local PROJECTS="${PROJECTS:-$HOME/projects}"
-	PROJECTS="$PROJECTS/github.com"
-
-	# skip all command-line option, and make sure we're parsing the
-	# repository-to-clone argument
-	at=0
-	for i in $(seq 1 $COMP_CWORD); do
-		word=${COMP_WORDS[i]}
-		[[ $word == -* ]] && continue
-		at=$((at+1))
-		# if IARG == 2, we're being invoked as 'git get'. If so, make sure
-		# the second word truly is 'get', stop otherwise
-		[[ $IARG == 2 && $at == 1 && $word != "get" ]] && return
-	done
-	if [[ $at != $IARG ]]; then
-		return
-	fi
-	URL="${COMP_WORDS[COMP_CWORD]}"
+	# URLs beginning with . are local -- let the normal bash autocompletion deal with them
+	[[ $URL == .* ]] && return
 
 	# Allow fully qualified http[s]://github.com/org/repo and git@github.com:org/repo forms
 	if [[ $URL == "https://github.com/"* || $URL == "http://github.com/"* ]]; then
@@ -162,13 +115,16 @@ _git_get()
 		URL="${A[1]}"
 	fi
 
-	# more than one '/' character. this is malformed, so stop
+	# if more than one '/' character. this is not a valid github URL; stop.
 	[[ $URL == */*/* ]] && return
 
 	# see if we're completing the org, or the repo part
 	if [[ $URL != */* ]]; then
 		# completing the org: show a list of already cloned orgs
-		WORDS=( $(ls $PROJECTS) )
+		local PROJECTS="${PROJECTS:-$HOME/projects}"
+		PROJECTS="$PROJECTS/github.com"
+
+		WORDS=( $(ls "$PROJECTS") )
 		WORDS=( "${WORDS[@]/%//}" )
 	else
 		# completing the repo: offer a list of repos available on github
@@ -196,6 +152,111 @@ _git_get()
 	done
 }
 
+# If COMP_CWORD is a positional argument, set $argidx to its index
+# ignoring any options that may have been specified before it.
+#
+# If COMP_CWORD is not a positional argument (e.g., it's an option
+# or an option's argument), set $argidx to empty
+#
+# arguments: list of options that admit an argument, suffixed by '='
+#
+# example:
+#     * assuming competion of `git clone --test foo`
+#         __arg_index --foo= --bar= --baz=
+#       sets argidx=2
+#     * assuming competion of `git clone --test foo`
+#         __arg_index --test=
+#       sets argidx=
+#     * assuming competion of `git clone --test foo -k`
+#         __arg_index --test=
+#       sets argidx=
+#     * assuming competion of `git clone --test foo -k bar`
+#         __arg_index --test=
+#       sets argidx=2
+#
+__arg_index()
+{
+	local c p i idx=0
+	for i in $(seq 1 $COMP_CWORD); do
+		p=${COMP_WORDS[i-1]}	# previous word
+		c=${COMP_WORDS[i]}	# current word
+		o=1			# was this word an option (begin by assuming it was)?
+
+		# an option
+		[[ $c == -* ]] && continue
+
+		# an argument of a previously specified option
+		[[ $p == -* && p != -*=* ]] && (echo "$@" | grep -qw -- "$p=") && continue
+
+		# a positional argument
+		o=0
+		idx=$((idx+1))
+	done
+
+	# set $argidx only if the current word was an argument
+	[[ $o == 0 ]] && argidx=$idx || argidx=
+}
+
+############
+
+# _git_clone enhancements
+
+# Do we have git autocomplete?
+if declare -F _git > /dev/null; then
+	# Have we already (not) monkey-patched it?
+	if  ! declare -F _git_clone_without_get > /dev/null; then
+		# Duplicate and rename the '_git_clone' function
+		eval "$(declare -f _git_clone | sed 's/_git_clone/_git_clone_without_get/')"
+	fi
+else
+	# no git autocompletions; add shims and declare autocompletion
+	__git() { git "$@"; }
+	_git_clone_without_get() { : ; }
+	
+	TODO: declare autocompletion
+fi
+
+# redefine _git_clone to auto-complete the first positional argument
+_git_clone()
+{
+	# try standard completions, return if successful
+	_git_clone_without_get
+	[[ ${#COMPREPLY[@]} -gt 0 ]] && return
+
+	# stop if we're completing an option
+	[[ $cur == -* ]] && return
+
+	# stop if we're completing an option's argument (e.g., '--config cfgfile')
+	local argopts=$(__git clone --git-completion-helper)
+	echo "$argopts" | grep -qw -- "$prev=" && return
+
+	# see if we're completing the second positional argument ('git clone <URL>')
+	__arg_index "$argopts"
+	[[ $argidx -ne 2 ]] && return
+
+	# attempt URL completion
+#	echo $'\n'"HERE! cur='$cur' COMP_CWORD=$COMP_CWORD argidx=$argidx"$'\n'
+	_complete_url "$cur"
+}
+
+# git's autocompletion scripts will automatically invoke _git_get() for 'get' subcommand
+_git_get()
+{
+	# see if we're completing the positional argument corresponding to the URL
+	local prog=$(basename ${COMP_WORDS[0]})
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+
+	__arg_index
+
+	# 'git-get <URL>'
+	[[ $prog == "git-get" && $argidx -eq 1 ]] && { _complete_url "$cur"; return; }
+
+	# ('git get <URL>' or 'git-get <URL>')
+	[[ $argidx -eq 2 ]] && { _complete_url "$cur"; return; }
+}
+
+############
+
 #
 # Install completions
 #
@@ -215,3 +276,57 @@ fi
 # Enable completion for git-get even when not called as a git subcommand
 complete -o bashdefault -o default -o nospace -F _git_get git-get 2>/dev/null \
 	|| complete -o default -o nospace -F _git_get git-get
+
+
+############
+
+#
+# Unit tests
+#
+
+_git_get_unit_test()
+{
+	for cmd in "git-get" "git get"; do
+		for opts in "" "-s --long"; do
+			# test succesful completions
+			COMPREPLY_TRUE=( "mjuric/lsd " "mjuric/lsd-setup " )
+			for url in "mjuric/lsd" "git@github.com:mjuric/lsd" "https://github.com/mjuric/lsd" "http://github.com/mjuric/lsd"; do
+				COMPREPLY=()
+				COMP_WORDS=($cmd $opts $url)
+				COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1)) _git_get
+				# compare the output vs expectation
+				diff=$(diff <(printf "%s\n" "${COMPREPLY_TRUE[@]}") <(printf "%s\n" "${COMPREPLY[@]}"))
+				[[ -n "$diff" ]] && { echo "[🛑] error: ${COMP_WORDS[@]} returned wrong completions" $'\n' "$diff"; return; }
+				echo "[✔] ${COMP_WORDS[@]}"
+			done
+
+			# test completions that should fail
+			for url in "./mjuric/lsd" "a/b/c" "git@github.com"; do
+				COMPREPLY=()
+				COMP_WORDS=($cmd $opts $url)
+				COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1)) _git_get
+				[[ ${#COMPREPLY[@]} == 0 ]] || { echo "[🛑] error: ${COMP_WORDS[@]} should've returned zero completions"; printf "%s\n" "${COMPREPLY[@]}"; return; }
+				echo "[✔] ${COMP_WORDS[@]}"
+			done
+		done
+	done
+}
+
+_arg_index_unit_test()
+{
+	COMP_WORDS=(git clone --test foo)
+	COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1))
+	__arg_index "--foo= --bar= --baz="; [[ $argidx == 2 ]] || { echo "test failed at line $LINENO"; return; }
+	__arg_index --test=;                [[ -z $argidx ]]   || { echo "test failed at line $LINENO"; return; }
+
+	COMP_WORDS=(git clone --test foo -k)
+	COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1))
+	__arg_index --foo= --bar= --baz=;   [[ -z $argidx ]]   || { echo "test failed at line $LINENO"; return; }
+
+	COMP_WORDS=(git clone --test foo -k bar)
+	COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1))
+	__arg_index --test=;                [[ $argidx == 2 ]] || { echo "test failed at line $LINENO"; return; }
+	
+	echo "[✔] __arg_index unit tests succeeded."
+}
+
